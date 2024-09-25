e84c0f3bc5e958f1e861872d0a6ca55bc3fbb42c
Soheil Ghanbary <soli.ghanbary@gmail.com>
2024-09-24 18:12:00 -0700

    update title

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default () => {
   return (
     <section className="flex h-dvh w-dvw flex-col items-center justify-center gap-4 p-4 text-center">
       <h1 className="bg-gradient-to-r from-gray-700 to-gray-900 bg-clip-text font-black text-3xl text-transparent lg:text-5xl dark:from-gray-300 dark:to-gray-500">
-        Welcome to Nix Stack
+        Welcome to Nix Starter
       </h1>
       <p>The Modern Next.js Stack</p>
       <Button asChild className="rounded-full">
