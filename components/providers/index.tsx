b458dc1a79685105bdc41b35e9b3dd9ae1d178a8
Soheil Ghanbary <soli.ghanbary@gmail.com>
2024-10-24 02:17:54 -0700

    add nuqs lib

--- components/providers/index.tsx
+++ components/providers/index.tsx
@@ -1,16 +1,19 @@
 'use client';
 import { ThemeProvider } from 'next-themes';
+import { NuqsAdapter } from 'nuqs/adapters/next/app';
 import type { PropsWithChildren } from 'react';
 import { QueryProvider } from './query-provider';
 import { ToastProvider } from './toast-provider';
 
 export default function Providers({ children }: PropsWithChildren) {
   return (
-    <QueryProvider>
-      <ThemeProvider attribute="class" enableColorScheme defaultTheme="light">
-        {children}
-        <ToastProvider />
-      </ThemeProvider>
-    </QueryProvider>
+    <NuqsAdapter>
+      <QueryProvider>
+        <ThemeProvider attribute="class" enableColorScheme defaultTheme="light">
+          {children}
+          <ToastProvider />
+        </ThemeProvider>
+      </QueryProvider>
+    </NuqsAdapter>
   );
 }
