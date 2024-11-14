c75b2efd95dafc53deace9b977966e5f9a69eb1f
Soheil Ghanbary <soli.ghanbary@gmail.com>
2024-11-14 11:58:33 +0330

    update env example

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  env: {
-    LIARA_ENDPOINT: process.env.LIARA_ENDPOINT,
-    LIARA_BUCKET_NAME: process.env.LIARA_BUCKET_NAME,
-    LIARA_ACCESS_KEY: process.env.LIARA_ACCESS_KEY,
-    LIARA_SECRET_KEY: process.env.LIARA_SECRET_KEY,
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
